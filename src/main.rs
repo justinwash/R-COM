936f50eb8c8e82306255fd3b9b8ee1ec5f0c207f
Justin Wash <justin.wash@pm.me>
2019-08-28 10:28:13 -0500

    types, ui, squad

--- src/main.rs
+++ src/main.rs
@@ -3,70 +3,175 @@ extern crate rand;
 
 use self::crossterm::{ execute, input, style, AsyncReader, 
     Clear, ClearType, Color, Colorize, Command, Crossterm, Goto, 
-    InputEvent, KeyEvent, Output, PrintStyledFont, RawScreen, Show,
+    InputEvent, KeyEvent, Output, PrintStyledFont, RawScreen, Show, TerminalCursor
 };
 
 use std::io::{stdout, Write};
 
-
 fn main() {
-    init();
-
-    // screen has to be in raw mode in order for the key presses not to be printed to the screen.
     let _raw = RawScreen::into_raw_mode();
     let crossterm = Crossterm::new();
 
+    init_ui();
+    init_map();
+
+    let origin = get_cursor_origin(crossterm.cursor());
+    let game_state = Mode::Select(Select::None);
+
     let mut stdin = crossterm.input().read_async();
 
-    // start the game loop;
-    loop {
-        let pressed_key = stdin.next();
+    let squad = v
 
-        if let Some(InputEvent::Keyboard(key)) = pressed_key {
-            match key {
-                KeyEvent::Up => { crossterm.cursor().move_up(1); }
-                KeyEvent::Down => { crossterm.cursor().move_down(1); }
-                KeyEvent::Left => { crossterm.cursor().move_left(1); }
-                KeyEvent::Right => { crossterm.cursor().move_right(1); }
-                _ => { },
-            };
+    loop {
+        match game_state {
+            Mode::Deploy => {
+                update_cursor(&mut stdin, crossterm.cursor(), origin);
+
+            }
+            Mode::Select(Select::None) => {
+                update_cursor(&mut stdin, crossterm.cursor(), origin);
+            }
+            _ => { }
         }
     }
 }
 
-fn init() {
+fn init_ui() {
     execute!(
         stdout(),
         Clear(ClearType::All),
         Goto(0, 0),
-        PrintStyledFont(style(format!("{}", BORDERS.join("\n\r"))).with(Color::Red)),
-        Goto(1, 14),
-        PrintStyledFont(style(format!("{}", COMMANDS.join("\n\r"))).with(Color::White)),
+        PrintStyledFont(style(format!("{}", BORDERS.join("\n\r"))).with(Color::White)),
         Show
-    );
+    ).unwrap();
+}
+
+fn init_map() {
+    // max map dimensions: 12x48
+    // execute!(stdout(), Goto(3, 3), PrintStyledFont(style(format!("{}", crossterm.cursor().pos()).with(Color::White)), Show).unwrap();
+}
+
+fn get_cursor_origin(cursor: TerminalCursor) -> (u16, u16) {
+    execute!(stdout(), Goto(0, 0),Show).unwrap();
+    let origin = cursor.pos();
+    execute!(stdout(), Goto(2, 2),Show).unwrap();
+    return origin;
+}
+
+fn update_cursor(input: &mut AsyncReader, mut cursor: TerminalCursor, origin: (u16, u16)) {
+    let pressed_key = input.next();
+
+    if let Some(InputEvent::Keyboard(key)) = pressed_key {
+        let (cursor_x, cursor_y) = cursor.pos();
+        match key {
+            KeyEvent::Up => { if cursor_y > origin.1 + 2 { cursor.move_up(1); } }
+            KeyEvent::Down => { if cursor_y < origin.1 + 14 { cursor.move_down(1); } }
+            KeyEvent::Left => { if cursor_x > 2 { cursor.move_left(1); } }
+            KeyEvent::Right => { if cursor_x < 50 { cursor.move_right(1); } }
+            _ => { },
+        };
+    }
 }
 
 
 
-const BORDERS: [&str; 16] = [
-    "╔═╡ R-COM ╞════════════════════════════════════════════════════════════╗",
-    "║                                                                      ║",
-    "║                                                                      ║",
-    "║                                                                      ║",
-    "║                                                                      ║",
-    "║                                                                      ║",
-    "║                                                                      ║",
-    "║                                                                      ║",
-    "║                                                                      ║",
-    "║                                                                      ║",
-    "║                                                                      ║",
-    "║                                                                      ║",
-    "║                                                                      ║",
-    "╠═══╡ Commands ╞═══════════════════════════════════════════════════════╣",
-    "║                                                                      ║",
-    "╚══════════════════════════════════════════════════════════════════════╝",
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const BORDERS: [&str; 19] = [
+    "╔═╡ R-COM ╞═════════════════════════════════════════╦═╡ Status ╞══════╗",
+    "║                                                   ║                 ║",
+    "║                                                   ║                 ║",
+    "║                                                   ║                 ║",
+    "║                                                   ║                 ║",
+    "║                                                   ║                 ║",
+    "║                                                   ║                 ║",
+    "║                                                   ║                 ║",
+    "║                                                   ║                 ║",
+    "║                                                   ║                 ║",
+    "║                                                   ║                 ║",
+    "║                                                   ║                 ║",
+    "║                                                   ║                 ║",
+    "║                                                   ║                 ║",
+    "║                                                   ║                 ║",
+    "║                                                   ║                 ║",
+    "╠═══╡ Commands ╞════════════════════════════════════╩═════════════════╣",
+    "║                                                                     ║",
+    "╚═════════════════════════════════════════════════════════════════════╝",
 ];
 
-const COMMANDS: [&str; 1] = [
-        " Move    Aim     Use Item     Hunker Down     Reload    Switch Weapon",
-];
+struct Loop { }
+impl Loop {
+    pub fn select(mut self) {
+        loop {
+
+        }
+    }
+}
+
+enum Mode {
+    Deploy,
+    EnemyTurn,
+    Select(Select),
+    Action(Action),
+}
+
+enum Select {
+    None,
+    Terrain,
+    Object,
+    Enemy,
+    Soldier
+}
+
+enum Action {
+    Move,
+    Fire,
+    Sprint,
+    UseItem,
+    HunkerDown,
+    Reload,
+    ChangeWeapon
+}
+
+struct Position {
+    x: u16,
+    y: u16
+}
+
+
+struct Soldier {
+    name: String,
+    hp: u16,
+    pos: Position
+}
+
+impl Soldier {
+    fn new(name: String, pos: Position) -> Soldier {
+         return Soldier {
+             name: name,
+             hp: 10,
+             pos: pos
+         }
+    }
+}
