db1d3441e21b9bfec17f897e196864c2d2eba807
Justin Wash <justin.wash@pm.me>
2019-08-28 10:30:07 -0500

    fix vector error

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,14 @@ fn main() {
 
     let mut stdin = crossterm.input().read_async();
 
-    let squad = v
+    let squad = vec![
+        Soldier::new(String::from("Jinpachi"), Position { x: 2, y: 2 }),
+        Soldier::new(String::from("Heihachi"), Position { x: 2, y: 3 }),
+        Soldier::new(String::from("Kazumi"), Position { x: 2, y: 4 }),
+        Soldier::new(String::from("Kazuya"), Position { x: 3, y: 2 }),
+        Soldier::new(String::from("Jun"), Position { x: 3, y: 3 }),
+        Soldier::new(String::from("Jin"), Position { x: 3, y: 4 }),
+    ];
 
     loop {
         match game_state {
